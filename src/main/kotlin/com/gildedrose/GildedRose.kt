d9bdb8bd7f3e57b04d192ed055983d095a937507
Kenny Cho <Kenny.Cho@softwire.com>
2024-06-25 17:01:11 +0100

    Added tests and refactored updateQuality

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,54 +2,48 @@ package com.gildedrose
 
 class GildedRose(var items: List<Item>) {
 
+    private fun updateQualityAndSellIn(item: Item, qualityChange: Int) {
+        item.quality += qualityChange
+        if (item.quality > 50) item.quality = 50
+        if (item.quality < 0) item.quality = 0
+
+        item.sellIn--
+    }
+
+    private fun setQualityAndDecrSellIn(item: Item, quality: Int) {
+        item.quality = quality
+        item.sellIn--
+    }
+
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+        /*
+        * Assumes "Conjured" can be combined with all other items, but other special items
+        * (Ex. aged brie, sulfuras, backstage passes) cannot combine with each other
+        * */
+        for (item in items) {
+
+            val conjured = item.name.contains("conjured", ignoreCase = true)
+            val conjuredMultiplier = if(conjured) 2 else 1
+            val expired = item.sellIn <= 0
+            val expiredMultiplier = if (expired) 2 else 1
+            val multiplier = conjuredMultiplier * expiredMultiplier
+
+            if (item.name.contains("aged brie", ignoreCase = true)) {
+                updateQualityAndSellIn(item, 1 * multiplier)
+            } else if (item.name.contains("sulfuras", ignoreCase = true)) {
+                // Do nothing
+            } else if (item.name.contains("backstage passes", ignoreCase = true)) {
+                if (expired) {
+                    setQualityAndDecrSellIn(item, 0)
+                } else {
+                    var increment = 0
+                    if (item.sellIn > 10) increment = 1
+                    else if (item.sellIn > 5) increment = 2
+                    else if (item.sellIn > 0) increment = 3
+                    updateQualityAndSellIn(item, increment * multiplier)
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
+                updateQualityAndSellIn(item, -1 * multiplier)
             }
         }
     }
